3af248810c5960517da83d1a564f0c090959765a
Maxwell Benton <maxwellbenton@gmail.com>
2025-09-06 12:05:56 -0400

    Initial Commit

---
+++ WebView Dreams/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebView Dreams
+//
+//  Created by Maxwell Benton on 9/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebView Dreams/WebView_DreamsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebView_DreamsApp.swift
+//  WebView Dreams
+//
+//  Created by Maxwell Benton on 9/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WebView_DreamsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
